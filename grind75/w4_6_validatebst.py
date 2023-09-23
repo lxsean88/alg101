b66d1abefe17127ab351f8684f5330ac71f3a0c8
Sean Lin <lxsean88@gmail.com>
2023-09-22 18:37:19 -0700

    add non-recursive way to validate BST

--- grind75/w4_6_validatebst.py
+++ grind75/w4_6_validatebst.py
@@ -9,3 +9,20 @@
             return isValidHelper(root.left, minvalue, root.val - 1) & isValidHelper(root.right, root.val + 1, maxvalue)
 
         return isValidHelper(root, -sys.maxsize, sys.maxsize)
+
+
+
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        stack = []
+        pre = - sys.maxsize
+        while root or stack:
+            while root:
+                stack.append(root)
+                root = root.left
+            
+            root = stack.pop()
+            if root.val <= pre:
+                return False
+            pre = root.val
+            root = root.right
+        return True
